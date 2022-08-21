9525f9e0cff9586fdabd98bf104a5fa1ff3bb58c
danemaide <maide.dane@trtworld.com>
2022-08-22 01:06:19 +0300

    MVVM

--- app/src/main/java/com/maidedane/todoapp/data/model/Todo.kt
+++ app/src/main/java/com/maidedane/todoapp/data/model/Todo.kt
@@ -1,4 +1,14 @@
 package com.maidedane.todoapp.data.model
 
-class Todo {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Todo(
+    val todo:String,
+    val description:String,
+
+    @PrimaryKey(autoGenerate = true)
+    val id:Int=0
+) {
 }
--- app/src/main/java/com/maidedane/todoapp/data/room/TodoDao.kt
+++ app/src/main/java/com/maidedane/todoapp/data/room/TodoDao.kt
@@ -1,4 +1,21 @@
 package com.maidedane.todoapp.data.room
 
+import androidx.room.*
+import com.maidedane.todoapp.data.model.Todo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface TodoDao {
+
+    @Query("SELECT * FROM todo")
+    fun getTodo(): Flow<List<Todo>>
+
+    @Query("SELECT * FROM todo WHERE id=:id")
+    suspend fun getTodoById(id:Int): Todo
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTodo(todo: Todo)
+
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
 }
