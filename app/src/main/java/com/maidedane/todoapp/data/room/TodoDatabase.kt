bfdaaccf70d67ec6ee7942b553fa88104bafd46c
danemaide <maide.dane@trtworld.com>
2022-08-22 01:06:19 +0300

    model and room added

--- app/src/main/java/com/maidedane/todoapp/data/room/TodoDatabase.kt
+++ app/src/main/java/com/maidedane/todoapp/data/room/TodoDatabase.kt
@@ -1,3 +1,36 @@
 package com.maidedane.todoapp.data.room
 
-abstract class TodoDatabase
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.maidedane.todoapp.data.model.Todo
+
+@Database(
+
+    entities = [Todo::class],
+    version = 1
+)
+
+abstract class TodoDatabase : RoomDatabase() {
+
+    abstract val todoDao: TodoDao
+
+    //Singleton mantığı
+
+    companion object {
+
+        @Volatile
+        private var instance: TodoDatabase? = null
+
+        private val lock = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(lock) {
+            instance ?: makeDatabase(context).also { instance = it }
+        }
+
+        private fun makeDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext, TodoDatabase::class.java, "tododatabase"
+        ).build()
+    }
+}
